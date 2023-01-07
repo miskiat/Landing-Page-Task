bbdae50c338255993bcc6f51232c71e66d056904
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-01-07 11:41:55 +0100

    image

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,17 +1,7 @@
 import React from "react";
 import { houses } from "../data";
 import "./Home.css";
-import TravelEvent from "../components/Event";
-const events = [
-  [
-    {
-      icon: "public/house-icon.svg",
-      desc: "BROWSE ALL PHOTOS",
-    },
-    { icon: "/vectors/shoppingbag-icon.svg", desc: "SHOP OUR PRODUCTS" },
-    { icon: "/public/vectors/compass.svg", desc: "LEARN ABOUT THE PROJECT" },
-  ],
-];
+
 const Home = () => {
   return (
     <div className="home">
@@ -23,34 +13,44 @@ const Home = () => {
         <img className="globe" src="/vectors/globe-icon.svg" />
         Explore Locations
       </button>
-      <div>
-        <div className="houses">
-          {houses.map((house) => (
-            <img src={`/images/${house}`} alt="" />
-          ))}
-        </div>
+
+      <div className="houses">
+        {houses.map((house) => (
+          <img src={`/images/${house}`} alt="" />
+        ))}
+      </div>
+
+      <div className="second-section">
         <div>
           {" "}
           <p className="discovery">
             Discover 274 homes from 10 places in 4 countries
           </p>
-          <div className="event">
-            {events.map((data, index) => (
-              <div>
-                {data.map(({ icon, desc }, i) => (
-                  <TravelEvent key={i} icon={icon} desc={desc} />
-                ))}
-              </div>
-            ))}
-            {/* {events.map((event, index) => (
-              <events icon={event.icon} desc={event.desc} />
-            ))} */}
+        </div>
+        <div className="events">
+          <div className="event-browse">
+            <img src="/vectors/house-icon.svg" />
+            BROWSE ALL PHOTOS
+          </div>
+          <div className="event-product">
+            <img src="/vectors/shoppingbag-icon.svg" />
+            SHOP OUR PRODUCTS
+          </div>
+          <div className="event-project">
+            <img src="/vectors/compass.svg" />
+            LEARN ABOUT THE PROJECT
           </div>
         </div>
-        <div>World</div>
-        <p>Browse all the cities and countries</p>
-        <button>Europe</button>
-        <button>Oceania</button>
+
+        <div className="world">World</div>
+        <p className="browse">Browse all the cities and countries</p>
+        <div className="country-button">
+          <button className="europe">Europe</button>
+          <button className="oceania">Oceania</button>
+        </div>
+        <div>
+          <img src="/vectors/bourdeux.svg" />
+        </div>
       </div>
     </div>
   );
