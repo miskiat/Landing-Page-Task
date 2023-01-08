7f2b654af306ca3e5c4e4d977fcfb7b9140061c5
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-01-09 00:25:21 +0100

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,14 +5,16 @@ const Footer = () => {
   return (
     <div className="footer">
       <div>Houses OF THE World</div>
-      <h1>shop</h1>
-      <h1>About</h1>
-      <h1>Keep Updated</h1>
-      <h1>Credits</h1>
-      <h1>Terms</h1>
-      <div>
-        <img src="/vector/instagram.svg" />
-        <h1>Instagram</h1>
+      <div className="right">
+        <h1>shop</h1>
+        <h1>About</h1>
+        <h1>Keep Updated</h1>
+        <h1>Credits</h1>
+        <h1>Terms</h1>
+        <div>
+          <img src="/vector/instagram.svg" />
+          <h1>Instagram</h1>
+        </div>
       </div>
     </div>
   );
